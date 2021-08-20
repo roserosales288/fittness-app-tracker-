5308c2c697d19774a54e5527b7e8930208c173e1
Diamond Rosales <roserosales288@gmail.com>
2021-08-20 15:15:55 -0400

    added info

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect('mongodb://localhost/workout' ||process.env.MONGODB_URI,  {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
